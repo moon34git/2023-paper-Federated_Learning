ba4cf26be228d808acd453d15e83a336a5ce4e1c
Jihoon Moon <moon34git@gmail.com>
2023-04-12 20:54:51 +0900

    Update modality

--- multiFL/seismicFL.py
+++ multiFL/seismicFL.py
@@ -6,59 +6,49 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import CIFAR10
+from torch.utils.data import DataLoader, random_split, Dataset
+from sklearn.model_selection import train_test_split
 import pickle
-
+from tqdm import tqdm
 import flwr as fl
 
 os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
 os.environ['CUDA_VISIBLE_DEVICES'] = '2'
-
 DEVICE = torch.device("cuda")  # Try "cuda" to train on GPU
 print(
     f"Training on {DEVICE} using PyTorch {torch.__version__} and Flower {fl.__version__}"
 )
 
-NUM_CLIENTS = 2
-
-def load_datasets(num_clients: int):
-    # Download and transform CIFAR-10 (train and test)
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    )
-    trainset = CIFAR10("./data", train=True, download=True, transform=transform)
-    testset = CIFAR10("./data", train=False, download=True, transform=transform)
-
-    # Split training set into `num_clients` partitions to simulate different local datasets
-    partition_size = len(trainset) // num_clients
-    lengths = [partition_size] * num_clients
-    datasets = random_split(trainset, lengths, torch.Generator().manual_seed(42))
-
-    # Split each partition into train/val and create DataLoader
-    trainloaders = []
-    valloaders = []
-    for ds in datasets:
-        len_val = len(ds) // 10  # 10 % validation set
-        len_train = len(ds) - len_val
-        lengths = [len_train, len_val]
-        ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(42))
-        trainloaders.append(DataLoader(ds_train, batch_size=32, shuffle=True))
-        valloaders.append(DataLoader(ds_val, batch_size=32))
-    testloader = DataLoader(testset, batch_size=32)
-    return trainloaders, valloaders, testloader
+NUM_CLIENTS = 5
 
 def load_data(num_clients: int):
-    with open('/home/jhmoon/venvFL/2023-paper-Federated_Learning/Data/trainset5000.pickle', 'rb') as trs:
-        trainset = pickle.load(trs)
-    with open('/home/jhmoon/venvFL/2023-paper-Federated_Learning/Data/testset.pickle', 'rb') as tts:
-        testset = pickle.load(tts)
+    # Download and transform CIFAR-10 (train and test)
+    with open('/home/jhmoon/venvFL/2023-paper-Federated_Learning/Data/seismic.pickle', 'rb') as f:
+        data1 = pickle.load(f)
+    data1 = data1.iloc[:5000]
+    data1['class'] = data1['class'].apply(lambda x: 0 if x == 1 else (1 if x == 2 else 2)) 
 
-    partition_size = len(trainset) // num_clients
+    X = data1[[str(x) for x in range(50)]]
+    y = data1['class']
+    X = X.values
+    y = y.values
+    X = torch.tensor(X, dtype = torch.float32)
+    y = torch.tensor(y, dtype = torch.long)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=34)
+
+    torchvisionType_for_trainVal = []
+    for i in range(len(X_train)):
+        torchvisionType_for_trainVal.append((X_train[i], y_train[i]))
+
+    torchvisionType_for_test = []
+    for i in range(len(X_test)):
+        torchvisionType_for_test.append((X_test[i], y_test[i]))
+
+    partition_size = len(torchvisionType_for_trainVal) // num_clients
     lengths = [partition_size] * num_clients
-    datasets = random_split(trainset, lengths, torch.Generator().manual_seed(42))
+    datasets = random_split(torchvisionType_for_trainVal, lengths, torch.Generator().manual_seed(42))
 
-    # Split each partition into train/val and create DataLoader
     trainloaders = []
     valloaders = []
     for ds in datasets:
@@ -66,42 +56,27 @@ def load_data(num_clients: int):
         len_train = len(ds) - len_val
         lengths = [len_train, len_val]
         ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(42))
-        trainloaders.append(DataLoader(ds_train, batch_size=32, shuffle=True))
-        valloaders.append(DataLoader(ds_val, batch_size=32))
-    testloader = DataLoader(testset, batch_size=32)
+        trainloaders.append(DataLoader(ds_train, batch_size = 32, shuffle=True))
+        valloaders.append(DataLoader(ds_val, batch_size = 32))
+    testloader = DataLoader(torchvisionType_for_test, batch_size = 32)
     return trainloaders, valloaders, testloader
 
-class Net_former(nn.Module):
+
+class FCNet(nn.Module):
     def __init__(self):
-        super(Net_former, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size = (5, 5), stride = (1, 1), padding = 2)
-        self.pool1 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
-
-        self.conv2 = nn.Conv2d(32, 64, kernel_size = (2, 2), padding = 1)
-        self.pool2 = nn.MaxPool2d(kernel_size = (2, 2))
-
-        self.drop1 = nn.Dropout(0.25)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(3136, 1000)
-        self.drop2 = nn.Dropout(0.25)
-        self.fc2 = nn.Linear(1000, 10)
+        super(FCNet, self).__init__()
+        self.fc1 = nn.Linear(50, 32)
+        self.fc2 = nn.Linear(32,32)
+        self.fc3 = nn.Linear(32,3)
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = F.relu(x)
-
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = F.relu(x)
-
-        x = self.drop1(x)
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.drop2(x)
-        x = self.fc2(x)
-
-        return F.softmax(x, dim = 0)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
     
 def get_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
@@ -118,17 +93,17 @@ def train(net, trainloader, epochs: int):
     net.train()
     for epoch in range(epochs):
         correct, total, epoch_loss = 0, 0, 0.0
-        for images, labels in trainloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
+        for x, y in trainloader:
+            x, y = x.to(DEVICE), y.to(DEVICE)
             optimizer.zero_grad()
-            outputs = net(images)
-            loss = criterion(net(images), labels)
+            output = net(x)
+            loss = criterion(net(x), y)
             loss.backward()
             optimizer.step()
             # Metrics
             epoch_loss += loss
-            total += labels.size(0)
-            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+            total += y.size(0)
+            correct += (output.argmax(1) == y).sum().item()
         epoch_loss /= len(trainloader.dataset)
         epoch_acc = correct / total
         print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
@@ -139,13 +114,13 @@ def test(net, testloader):
     correct, total, loss = 0, 0, 0.0
     net.eval()
     with torch.no_grad():
-        for images, labels in testloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
+        for x, y in testloader:
+            x, y = x.to(DEVICE), y.to(DEVICE)
+            outputs = net(x)
+            loss += criterion(outputs, y).item()
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
     loss /= len(testloader.dataset)
     accuracy = correct / total
     return loss, accuracy
@@ -185,7 +160,7 @@ class FlowerClient(fl.client.NumPyClient):
         return float(loss), len(self.valloader), {"accuracy": float(accuracy)}
 
 def client_fn(cid) -> FlowerClient:
-    net = Net_former().to(DEVICE)
+    net = FCNet().to(DEVICE)
     trainloader = trainloaders[int(cid)]
     valloader = valloaders[int(cid)]
     return FlowerClient(cid, net, trainloader, valloader)
@@ -195,7 +170,7 @@ def evaluate(
     parameters: fl.common.NDArrays,
     config: Dict[str, fl.common.Scalar],
 ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
-    net = Net_former().to(DEVICE)
+    net = FCNet().to(DEVICE)
     valloader = valloaders[0]
     set_parameters(net, parameters)  # Update model with the latest parameters
     loss, accuracy = test(net, valloader)
@@ -203,29 +178,30 @@ def evaluate(
     return loss, {"accuracy": accuracy}
 
 def fit_config(server_round: int):
-    """Return training configuration dict for each round.
+    
+    # config = {
+    #     "server_round": server_round,  # The current round of federated learning
+    #     "local_epochs": 1 if server_round < 2 else 2,  #
+    # }
 
-    Perform two rounds of training with one local epoch, increase to two local
-    epochs afterwards.
-    """
     config = {
         "server_round": server_round,  # The current round of federated learning
-        "local_epochs": 1 if server_round < 2 else 2,  #
+        "local_epochs": 5,  #
     }
     return config
 
 # trainloaders, valloaders, testloader = load_datasets(NUM_CLIENTS)
 trainloaders, valloaders, testloader = load_data(NUM_CLIENTS)
 # Create an instance of the model and get the parameters
-params = get_parameters(Net_former())
+params = get_parameters(FCNet())
 
 # Pass parameters to the Strategy for server-side parameter initialization
 strategy = fl.server.strategy.FedAvg(
-    fraction_fit=1,
-    fraction_evaluate=2,
-    min_fit_clients=2,
-    min_evaluate_clients=2,
-    min_available_clients=NUM_CLIENTS,
+    fraction_fit=0.5,
+    fraction_evaluate= 3,
+    min_fit_clients= 3,
+    min_evaluate_clients=3,
+    min_available_clients=3,
     initial_parameters=fl.common.ndarrays_to_parameters(params),
     evaluate_fn=evaluate,
     on_fit_config_fn = fit_config,
@@ -240,7 +216,7 @@ if DEVICE.type == "cuda":
 fl.simulation.start_simulation(
     client_fn=client_fn,
     num_clients=NUM_CLIENTS,
-    config=fl.server.ServerConfig(num_rounds=5),  # Just three rounds
+    config=fl.server.ServerConfig(num_rounds=3),  # Just three rounds
     strategy=strategy,
     client_resources=client_resources,
 )
